9f14fdd571c9b3acfb404fb6c8e06ec10c6dfbae
Rachanapk <52086583+Rachanapk@users.noreply.github.com>
2023-05-24 04:05:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.4.6" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -14,6 +14,6 @@ provider "aws" {
 	}
 
 resource "aws_instance" "Raminfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
+	ami = "ami-0889a44b331db0194" 
 	instance_type = "t2.micro"
 }
